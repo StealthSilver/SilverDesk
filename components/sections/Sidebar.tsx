56543112a474bf43871261818abca6f562361694
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-14 18:55:17 +0530

    social links

--- components/sections/Sidebar.tsx
+++ components/sections/Sidebar.tsx
@@ -8,14 +8,14 @@ const Sidebar = () => {
   return (
     <aside className="fixed right-0 top-0 h-screen w-16 border-l border-[#21201f] flex flex-col items-center justify-center gap-8 text-[#21201f] shadow-lg">
       <Link
-        href="mailto:example@email.com"
+        href="mailto:intersectinglines1005@mail.com"
         target="_blank"
         className="hover:text-[#e34e30] transition"
       >
         <Mail className="w-6 h-6" />
       </Link>
       <Link
-        href="https://instagram.com"
+        href="https://www.instagram.com/_intersectinglines/"
         target="_blank"
         className="hover:text-[#e34e30] transition"
       >
